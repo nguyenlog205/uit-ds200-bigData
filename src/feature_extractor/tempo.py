547cbddee24ab06a54ce6d9a2f2a20fecce68145
nguyenTram05 <tram10102005@gmail.com>
2026-03-26 06:42:12 +0700

    using library librosa for cyclic tempogram and STFT chromagram

--- src/feature_extractor/tempo.py
+++ src/feature_extractor/tempo.py
@@ -1,8 +1,23 @@
 import numpy as np
 import librosa
-from matplotlib.ticker import ScalarFormatter
 import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
+"""
+Cyclic Tempogram Module
 
+Module encodes the tempo of an audio signal over time by analyzing rythmic information and mapping it using a Fourier transform
+
+Parameters:
+    sr: int, optinal
+        Sampling rate (default: 44100)
+    hop_length: int, optinal
+        hop_length between frames (default: 512)
+    win_length: int, optional 
+        win_length for tepogram in frames (default: 384)
+
+Returns:
+    np.ndarray: 2D array of shape (win_length // 2 + 1, time_frames) representing the cyclic tempogram.
+"""
 class CyclicTempogram:
     def __init__(self, sr=44100, hop_length=512, win_length=384):
         self.sr = sr 
@@ -10,39 +25,47 @@ class CyclicTempogram:
         self.win_length = win_length
 
     def extract(self, audio):
-        # tính onset strength O(m) 
         onset_env = librosa.onset.onset_strength(y=audio, sr=self.sr, hop_length=self.hop_length)
-        pad_len = self.win_length // 2
-        onset_pad = np.pad(onset_env, (pad_len, pad_len), mode='constant')
-        tempogram_cols = []
-        for m in range(len(onset_env)):
-            o_frame = onset_pad[m : m + self.win_length]
-            o_frame = o_frame - np.mean(o_frame)
-            # dùng cửa sổ Hanning để làm mịn 2 đầu audio
-            o_frame = o_frame * np.hanning(self.win_length)
-            # tính autocorrelation
-            r_tau = np.correlate(o_frame, o_frame, mode="full")[self.win_length - 1:]
-            # fourier Transform
-            tc_f = np.fft.rfft(r_tau)
-            magnitude = np.abs(tc_f)
-            # bỏ tần số thấp, nhiễu
-            magnitude[:3] = 0
-            tempogram_cols.append(magnitude)
-        tempogram_matrix = np.array(tempogram_cols).T
-        return tempogram_matrix / np.max(tempogram_matrix)
+        tempo = librosa.feature.fourier_tempogram(
+            onset_envelope = onset_env,
+            sr = self.sr,
+            hop_length = self.hop_length,
+            win_length = self.win_length
+        )
+        magnitude = np.abs(tempo)
+        return magnitude
     
     def shape(self, tempo):
         print(f"Shape of output: {tempo.shape}")
     
-    def plot(self, feature_matrix, ax):
-        bpm_freqs = np.arange(feature_matrix.shape[0]) * (self.sr * 60 / (self.hop_length * self.win_length))
-        times = librosa.frames_to_time(np.arange(feature_matrix.shape[1]), sr=self.sr, hop_length=self.hop_length)
-        feature_matrix = feature_matrix[1:, :]
+    def plot(self, tempo):
+        """
+        x_coords và y_coords dùng để truyền times, bpm_freqs vào --> mapping từng pixel theo times và bpm_freqs
+        Hiển thị ma trận STFT_chroma.
+        - Trục x: Thời gian (Time).
+        - Trục y: tần số (BPM).
+        """
+        tempo = tempo[1: ,:]
+        bpm_freqs = librosa.fourier_tempo_frequencies(sr=self.sr, hop_length=self.hop_length, win_length=self.win_length)
+        times = librosa.times_like(tempo, sr=self.sr, hop_length=self.hop_length)
         bpm_freqs = bpm_freqs[1:]
-        ax.pcolormesh(times, bpm_freqs, feature_matrix, cmap="coolwarm", shading="nearest")
-        ax.set_ylabel("beats per minute (BPM) [-]", fontsize=11)
-        ax.set_yscale("log", base=2) 
-        ax.set_yticks([16, 64, 256])
-        ax.set_yticklabels(["16", "64", "256"], fontsize=10)
-        ax.get_yaxis().set_major_formatter(ScalarFormatter())
+        img = librosa.display.specshow(
+            tempo,
+            sr = self.sr,
+            hop_length = self.hop_length,
+            x_coords = times,
+            y_coords = bpm_freqs,
+            x_axis = "time",
+            cmap = "coolwarm"
+        )
+        ax = plt.gca()
+        ax.set_yscale("log", base=2)
         ax.set_ylim(16, 512)
+        ax.set_yticks([16, 64, 256])
+        ax.set_yticklabels(["16", "64", "256"])
+        ax.get_yaxis().set_major_formatter(ScalarFormatter())
+        ax.set_ylabel("beats per minute (BPM)[-]", fontsize=11)
+        ax.set_xlabel("time[s]", fontsize=11)
+        plt.title("Cyclic Tempogram")
+        plt.tight_layout()
+        plt.show()
